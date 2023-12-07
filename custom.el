f97a9f1b4d877c09465b89def7d08ad3822a6121
Ln Gnmn <lngnmn2@yahoo.com>
2023-12-07 07:50:41 +0545

    crap

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(blink-cursor-mode nil)
  '(current-language-environment "UTF-8")
  '(custom-safe-themes
-   '("2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
+   '("4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(display-line-numbers-type nil)
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(menu-bar-mode nil)
